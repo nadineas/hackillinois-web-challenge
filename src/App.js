8dae6ce558f8513c7001744a82e8081d476171e4
Siddharta, Nadine Amanda <nadines3@illinois.edu>
2020-09-28 04:11:58 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styles from './styles.module.sass';
+
 
 function App() {
+  let [events, setEvents] = useState([]);
+  useEffect(() => {
+    fetch('https://api.hackillinois.org/event/').then(res => res.json()).then(json => {
+      setEvents(json.events)
+    })
+  }, [])
+  events.startTime = new Date(events.startTime);
+  let [times, setTime] = useState([]);
+  useEffect(() => { fetch('https://api.hackillinois.org/event/').then(res => res.json()).then(json => setTime(json.events.startTime)) })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.background}>
+      <div className={styles.displayinternal}>
+        {events.map(event => (
+          <div className={styles.display}>
+            <b className={styles.title}><u>{event.name}</u></b>
+            <i className={styles.time}>{event.startTime}</i>
+            <p>{event.description}</p>
+            <p>____________________________________________________________________________</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
